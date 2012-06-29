9883433c6d312cbbf8e23ff0fa3b64dc159b1cf8
ktf <not.discostoo@gmail.com>
2012-06-29 12:57:22 -0700

    init the db

--- flaskr.py
+++ flaskr.py
@@ -1,4 +1,5 @@
 import sqlite3
+from contextlib import closing
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
 
@@ -19,6 +20,13 @@ app.config.from_envvar('FLASKR_SETTINGS', silent=True) # override!
 
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
+
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
 
 
 if __name__ == '__main__':
