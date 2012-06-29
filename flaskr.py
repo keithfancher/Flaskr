922f322bc6bda2050b4dfaa12f88475f6c6604ce
ktf <not.discostoo@gmail.com>
2012-06-29 12:41:13 -0700

    oh hello

---
+++ flaskr.py
@@ -0,0 +1,25 @@
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, \
+     abort, render_template, flash
+
+
+# config
+DATABASE = '/tmp/flaskr.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+
+
+# create app!
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config.from_envvar('FLASKR_SETTINGS', silent=True) # override!
+
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+
+if __name__ == '__main__':
+    app.run()
